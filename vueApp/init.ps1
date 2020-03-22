018e31125e3fb997068cd1f43d23d7decef965c7
atilm <thielmann.andreas@googlemail.com>
2020-03-22 17:57:23 +0100

    added vue app dependencies

---
+++ vueApp/init.ps1
@@ -0,0 +1,7 @@
+New-Item -Path . -Name ".bablerc.js" -ItemType "file"
+New-Item -Path . -Name "webpack.config.js" -ItemType "file"
+New-Item -Path . -Name "src" -ItemType "directory"
+
+npm init
+npm install vue vue-loader vue-template-compiler webpack webpack-cli webpack-dev-server babel-loader @babel/core @babel/preset-env css-loader vue-style-loader html-webpack-plugin rimraf -D
+npm install bootstrap
