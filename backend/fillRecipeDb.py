eb75574d072e1fe9340375603b0fd8d4e7e0d9b2
atilm <thielmann.andreas@googlemail.com>
2020-04-03 11:30:03 +0200

    added detailed mock recipe

--- backend/fillRecipeDb.py
+++ backend/fillRecipeDb.py
@@ -7,7 +7,13 @@ recipeA.tags = ["Kuchen", "belgisch", "vegetarisch"]
 
 recipeB = Recipe()
 recipeB.name = "Lasagne"
+recipeB.numberOfPeople = 4
+recipeB.add_ingredient(None, "Lasagneblätter", 500, "g")
+recipeB.add_ingredient(None, "Hackfleischsoße", 1000, "ml")
+recipeB.add_ingredient(None, "Bechamelsoße", 500, "ml")
+recipeB.add_ingredient(None, "Käse", 200, "g")
 recipeB.tags = ["Hauptgericht", "italienisch"]
+recipeB.instructions = "Alles einschichten. Dann die Lasagne bei 200 °C eine viertel bis halbe Stunde backen. Den Parmesankäse erst 10 min vor Schluss auf die Lasagne geben und mit Olivenöl betreufeln."
 
 repo = RecipeRepository("recipeStore.json")
 repo.save(recipeA)
--- vueApp/src/views/RecipeDetailsView.vue
+++ vueApp/src/views/RecipeDetailsView.vue
@@ -1,5 +1,11 @@
 <template>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Details of recipe {{$route.params.id}}</h1>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
