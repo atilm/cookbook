b557dec23f57f47ca02eef0f19cdf0dc0c656314
atilm <thielmann.andreas@googlemail.com>
2020-04-19 13:05:36 +0200

    interpret recipe instructions as markdown

--- vueApp/src/views/RecipeDetailsView.vue
+++ vueApp/src/views/RecipeDetailsView.vue
@@ -13,21 +13,23 @@
                         </tr>
                     </tbody>
                 </table>
-                <p>{{currentRecipe.instructions}}</p>
+                <span v-html="currentInstructions"></span>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import RecipeService from "../common/recipeService"
-import Recipe from "../common/recipe"
+import RecipeService from "../common/recipeService";
+import Recipe from "../common/recipe";
+import showdown from 'showdown';
 
 export default {
     name: "recipe-details-view",
     data() {
         return {
-            currentRecipe: new Recipe()
+            currentRecipe: new Recipe(),
+            currentInstructions: ""
         }
     },
     mounted() {
@@ -37,7 +39,14 @@ export default {
     methods: {
         loadRecipe: function(id){
             let vm = this;
-            this.service.get(id).then(recipe => vm.currentRecipe = recipe);
+            this.service.get(id).then(recipe => vm.parseRecipe(recipe));
+        },
+        parseRecipe: function(recipe){
+            let vm = this;
+            let markdownConverter = new showdown.Converter();
+
+            vm.currentRecipe = recipe;
+            vm.currentInstructions = markdownConverter.makeHtml(recipe.instructions);
         }
     }
 }
